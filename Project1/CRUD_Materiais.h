b857acbdae040b348c0608fe3758304ab9306f2e
Victor Barreira <victorbarreira94@gmail.com>
2018-05-03 15:19:39 -0300

    Nova versão CRUD Materiais

--- Project1/CRUD_Materiais.h
+++ Project1/CRUD_Materiais.h
@@ -37,8 +37,27 @@ namespace Project1 {
 	private: System::Windows::Forms::DataGridView^  dataGridView1;
 	protected:
 	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::TextBox^  textBox1;
+
 	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label4;
+
+	private: System::Windows::Forms::Label^  label3;
+
+	private: System::Windows::Forms::Button^  btn_Alterar;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Tipo;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  CustoUnidade;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  QtdEstoque;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::GroupBox^  groupBox2;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button1;
+
+
+
 
 	private:
 		/// <summary>
@@ -56,52 +75,181 @@ namespace Project1 {
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->btn_Alterar = (gcnew System::Windows::Forms::Button());
+			this->Tipo = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->CustoUnidade = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->QtdEstoque = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+				this->Tipo, this->CustoUnidade,
+					this->QtdEstoque
+			});
 			this->dataGridView1->Location = System::Drawing::Point(12, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(377, 221);
+			this->dataGridView1->Size = System::Drawing::Size(377, 215);
 			this->dataGridView1->TabIndex = 0;
 			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &CRUD_Materiais::dataGridView1_CellContentClick);
 			// 
 			// groupBox1
 			// 
-			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Controls->Add(this->comboBox2);
+			this->groupBox1->Controls->Add(this->comboBox1);
+			this->groupBox1->Controls->Add(this->btn_Alterar);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->label2);
 			this->groupBox1->Controls->Add(this->label1);
 			this->groupBox1->Location = System::Drawing::Point(395, 12);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(326, 221);
+			this->groupBox1->Size = System::Drawing::Size(336, 155);
 			this->groupBox1->TabIndex = 1;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Cadastro/Alteração";
+			this->groupBox1->Text = L"Dados Principais";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(18, 30);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(28, 13);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Nome";
+			this->label1->Text = L"Tipo";
 			// 
-			// textBox1
+			// textBox2
 			// 
-			this->textBox1->Location = System::Drawing::Point(141, 27);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(179, 20);
-			this->textBox1->TabIndex = 1;
+			this->textBox2->Location = System::Drawing::Point(123, 53);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(197, 20);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(18, 56);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(77, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Custo/unidade";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(18, 82);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(99, 13);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Unidade de medida";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(122, 19);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(113, 20);
+			this->textBox4->TabIndex = 7;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(17, 22);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(82, 13);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"Qtd em estoque";
+			// 
+			// btn_Alterar
+			// 
+			this->btn_Alterar->Location = System::Drawing::Point(162, 105);
+			this->btn_Alterar->Name = L"btn_Alterar";
+			this->btn_Alterar->Size = System::Drawing::Size(128, 38);
+			this->btn_Alterar->TabIndex = 4;
+			this->btn_Alterar->Text = L"Alterar";
+			this->btn_Alterar->UseVisualStyleBackColor = true;
+			// 
+			// Tipo
+			// 
+			this->Tipo->HeaderText = L"Tipo";
+			this->Tipo->Name = L"Tipo";
+			// 
+			// CustoUnidade
+			// 
+			this->CustoUnidade->HeaderText = L"Custo/Unidade";
+			this->CustoUnidade->Name = L"CustoUnidade";
+			// 
+			// QtdEstoque
+			// 
+			this->QtdEstoque->HeaderText = L"Quantidade em Estoque";
+			this->QtdEstoque->Name = L"QtdEstoque";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(123, 26);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(197, 21);
+			this->comboBox1->TabIndex = 8;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(123, 78);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(197, 21);
+			this->comboBox2->TabIndex = 9;
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->button2);
+			this->groupBox2->Controls->Add(this->button1);
+			this->groupBox2->Controls->Add(this->textBox4);
+			this->groupBox2->Controls->Add(this->label4);
+			this->groupBox2->Location = System::Drawing::Point(396, 174);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(335, 53);
+			this->groupBox2->TabIndex = 8;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Quantidade";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(241, 19);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(39, 23);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"+";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(286, 19);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(39, 23);
+			this->button2->TabIndex = 9;
+			this->button2->Text = L"-";
+			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// CRUD_Materiais
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(733, 249);
+			this->ClientSize = System::Drawing::Size(743, 240);
+			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->dataGridView1);
 			this->Name = L"CRUD_Materiais";
@@ -109,11 +257,14 @@ namespace Project1 {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void dataGridView1_CellContentClick(System::Object^  sender, System::Windows::Forms::DataGridViewCellEventArgs^  e) {
 	}
-	};
+
+};
 }
