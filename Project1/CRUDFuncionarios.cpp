0fd88176eb7017f0573f8239df96945af8e50277
Victor Barreira <victorbarreira94@gmail.com>
2018-05-04 10:51:31 -0300

    CRUD Funcionários (1ª versão)

---
+++ Project1/CRUDFuncionarios.cpp
@@ -0,0 +1,2 @@
+#include "CRUDFuncionarios.h"
+
--- Project1/CRUDFuncionarios.h
+++ Project1/CRUDFuncionarios.h
@@ -10,12 +10,12 @@ namespace Project1 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Sumário para CRUDEquipes
+	/// Sumário para CRUDFuncionarios
 	/// </summary>
-	public ref class CRUDEquipes : public System::Windows::Forms::Form
+	public ref class CRUDFuncionarios : public System::Windows::Forms::Form
 	{
 	public:
-		CRUDEquipes(void)
+		CRUDFuncionarios(void)
 		{
 			InitializeComponent();
 			//
@@ -27,7 +27,7 @@ namespace Project1 {
 		/// <summary>
 		/// Limpar os recursos que estão sendo usados.
 		/// </summary>
-		~CRUDEquipes()
+		~CRUDFuncionarios()
 		{
 			if (components)
 			{
@@ -36,26 +36,26 @@ namespace Project1 {
 		}
 	private: System::Windows::Forms::DataGridView^  dataGridView1;
 	protected:
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  ID;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Nome;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  NumTrab;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  CustoDesloc;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  CustoHora;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  HorarioServ;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  CPF;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Salario;
 	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::TextBox^  textBox5;
 	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
 	private: System::Windows::Forms::TextBox^  textBox4;
 	private: System::Windows::Forms::Label^  label4;
 	private: System::Windows::Forms::TextBox^  textBox3;
 	private: System::Windows::Forms::Label^  label3;
 	private: System::Windows::Forms::TextBox^  textBox2;
 	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Button^  button4;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -71,11 +71,10 @@ namespace Project1 {
 		void InitializeComponent(void)
 		{
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->ID = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Nome = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->NumTrab = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->CustoDesloc = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->CustoHora = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->HorarioServ = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->CPF = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Salario = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
@@ -85,12 +84,11 @@ namespace Project1 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->button4 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
@@ -98,47 +96,42 @@ namespace Project1 {
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(5) {
-				this->Nome, this->NumTrab,
-					this->CustoDesloc, this->CustoHora, this->HorarioServ
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
+				this->ID, this->Nome,
+					this->CPF, this->Salario
 			});
 			this->dataGridView1->Location = System::Drawing::Point(12, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(546, 168);
-			this->dataGridView1->TabIndex = 0;
+			this->dataGridView1->Size = System::Drawing::Size(449, 193);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// ID
+			// 
+			this->ID->HeaderText = L"ID";
+			this->ID->Name = L"ID";
 			// 
 			// Nome
 			// 
 			this->Nome->HeaderText = L"Nome";
 			this->Nome->Name = L"Nome";
 			// 
-			// NumTrab
+			// CPF
 			// 
-			this->NumTrab->HeaderText = L"Número de Trabalhadores";
-			this->NumTrab->Name = L"NumTrab";
+			this->CPF->HeaderText = L"CPF";
+			this->CPF->Name = L"CPF";
 			// 
-			// CustoDesloc
+			// Salario
 			// 
-			this->CustoDesloc->HeaderText = L"Custo de Deslocamento";
-			this->CustoDesloc->Name = L"CustoDesloc";
-			// 
-			// CustoHora
-			// 
-			this->CustoHora->HeaderText = L"Custo por Hora";
-			this->CustoHora->Name = L"CustoHora";
-			// 
-			// HorarioServ
-			// 
-			this->HorarioServ->HeaderText = L"Horário de Serviço";
-			this->HorarioServ->Name = L"HorarioServ";
+			this->Salario->HeaderText = L"Salário";
+			this->Salario->Name = L"Salario";
 			// 
 			// groupBox1
 			// 
 			this->groupBox1->Controls->Add(this->button3);
 			this->groupBox1->Controls->Add(this->button2);
 			this->groupBox1->Controls->Add(this->button1);
-			this->groupBox1->Controls->Add(this->textBox5);
 			this->groupBox1->Controls->Add(this->label5);
+			this->groupBox1->Controls->Add(this->comboBox1);
 			this->groupBox1->Controls->Add(this->textBox4);
 			this->groupBox1->Controls->Add(this->label4);
 			this->groupBox1->Controls->Add(this->textBox3);
@@ -147,139 +140,130 @@ namespace Project1 {
 			this->groupBox1->Controls->Add(this->label2);
 			this->groupBox1->Controls->Add(this->textBox1);
 			this->groupBox1->Controls->Add(this->label1);
-			this->groupBox1->Location = System::Drawing::Point(12, 186);
+			this->groupBox1->Location = System::Drawing::Point(467, 12);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(546, 159);
-			this->groupBox1->TabIndex = 1;
+			this->groupBox1->Size = System::Drawing::Size(253, 193);
+			this->groupBox1->TabIndex = 2;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Dados da equipe";
+			this->groupBox1->Text = L"Dados gerais";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(6, 28);
+			this->label1->Location = System::Drawing::Point(6, 30);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(18, 13);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Nome";
+			this->label1->Text = L"ID";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(117, 25);
+			this->textBox1->Location = System::Drawing::Point(87, 27);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(143, 20);
+			this->textBox1->Size = System::Drawing::Size(159, 20);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(117, 51);
+			this->textBox2->Location = System::Drawing::Point(87, 53);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(143, 20);
+			this->textBox2->Size = System::Drawing::Size(160, 20);
 			this->textBox2->TabIndex = 3;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(6, 54);
+			this->label2->Location = System::Drawing::Point(6, 56);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(95, 13);
+			this->label2->Size = System::Drawing::Size(35, 13);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Horário de Serviço";
+			this->label2->Text = L"Nome";
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(117, 77);
+			this->textBox3->Location = System::Drawing::Point(87, 79);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(68, 20);
+			this->textBox3->Size = System::Drawing::Size(159, 20);
 			this->textBox3->TabIndex = 5;
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(6, 80);
+			this->label3->Location = System::Drawing::Point(6, 82);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(105, 13);
+			this->label3->Size = System::Drawing::Size(27, 13);
 			this->label3->TabIndex = 4;
-			this->label3->Text = L"Nº de Trabalhadores";
+			this->label3->Text = L"CPF";
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(397, 25);
+			this->textBox4->Location = System::Drawing::Point(87, 105);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(143, 20);
+			this->textBox4->Size = System::Drawing::Size(159, 20);
 			this->textBox4->TabIndex = 7;
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(292, 28);
+			this->label4->Location = System::Drawing::Point(6, 108);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(75, 26);
+			this->label4->Size = System::Drawing::Size(39, 13);
 			this->label4->TabIndex = 6;
-			this->label4->Text = L"Custo de\r\nDeslocamento";
+			this->label4->Text = L"Salário";
 			// 
-			// textBox5
+			// comboBox1
 			// 
-			this->textBox5->Location = System::Drawing::Point(397, 77);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(143, 20);
-			this->textBox5->TabIndex = 9;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(87, 132);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(159, 21);
+			this->comboBox1->TabIndex = 8;
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(292, 80);
+			this->label5->Location = System::Drawing::Point(6, 135);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(78, 13);
-			this->label5->TabIndex = 8;
-			this->label5->Text = L"Custo por Hora";
+			this->label5->Size = System::Drawing::Size(52, 13);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Categoria";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(156, 109);
+			this->button1->Location = System::Drawing::Point(9, 164);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 35);
+			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 10;
 			this->button1->Text = L"Cadastrar";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(237, 109);
+			this->button2->Location = System::Drawing::Point(90, 164);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 35);
+			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 11;
 			this->button2->Text = L"Alterar";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(318, 109);
+			this->button3->Location = System::Drawing::Point(171, 164);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 35);
+			this->button3->Size = System::Drawing::Size(75, 23);
 			this->button3->TabIndex = 12;
-			this->button3->Text = L"Funcionários da Equipe";
+			this->button3->Text = L"Excluir";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
-			// button4
-			// 
-			this->button4->Location = System::Drawing::Point(249, 351);
-			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(75, 40);
-			this->button4->TabIndex = 2;
-			this->button4->Text = L"Excluir";
-			this->button4->UseVisualStyleBackColor = true;
-			// 
-			// CRUDEquipes
+			// CRUDFuncionarios
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(574, 403);
-			this->Controls->Add(this->button4);
+			this->ClientSize = System::Drawing::Size(745, 216);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->dataGridView1);
-			this->Name = L"CRUDEquipes";
-			this->Text = L"Equipes";
+			this->Name = L"CRUDFuncionarios";
+			this->Text = L"Funcionários";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
